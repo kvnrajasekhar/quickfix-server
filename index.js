1b3fd4073eb861c38f47b2e4bbde8f37b556ae58
Kanagala Venkata Naga Rajasekhar <109467292+kvnrajasekhar@users.noreply.github.com>
2025-05-19 15:21:33 +0530

    updated to return data in IST

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const moment = require('moment-timezone'); 
 
 app.use(express.json());
 
 const MONGO_URL = process.env.MONGO_URL;
-console.log(MONGO_URL); 
+console.log(MONGO_URL);
 
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    credentials :true,
 }));
 
 mongoose.connect(MONGO_URL)
@@ -25,7 +25,7 @@ mongoose.connect(MONGO_URL)
 
 
 
-const Complaint = require('./modal/Complaint');
+const Complaint = require('./modal/Complaint'); 
 
 app.post('/complaint', async (req, res) => {
     try {
@@ -46,11 +46,16 @@ app.post('/complaint', async (req, res) => {
 
 app.get('/complaints', async (req, res) => {
     try {
-        const complaints = await Complaint.find();
-        res.status(200).json(complaints);
+        const complaints = await Complaint.find().lean(); 
+        const complaintsIST = complaints.map(complaint => ({
+            ...complaint,
+            createdAt: moment.utc(complaint.createdAt).tz('Asia/Kolkata').format(),
+            updatedAt: moment.utc(complaint.updatedAt).tz('Asia/Kolkata').format(),
+        }));
+        res.status(200).json(complaintsIST);
     } catch (error) {
         console.error("Error fetching all complaints:", error);
-        res.status(500).json({ error: 'Failed to fetch complaints.', error: error.message }); 
+        res.status(500).json({ error: 'Failed to fetch complaints.', error: error.message });
     }
 });
 
@@ -62,14 +67,18 @@ app.patch('/complaints/:id', async (req, res) => {
         const updatedComplaint = await Complaint.findByIdAndUpdate(
             complaintId,
             { status: newStatus, updatedAt: Date.now() },
-            { new: true }
-        );
+            { new: true, runValidators: true } //  Added runValidators
+        ).lean(); 
 
         if (!updatedComplaint) {
             return res.status(404).json({ message: 'Complaint not found' });
         }
+        const updatedComplaintIST = {
+          ...updatedComplaint,
+          updatedAt: moment.utc(updatedComplaint.updatedAt).tz('Asia/Kolkata').format()
+        }
 
-        res.status(200).json(updatedComplaint);
+        res.status(200).json(updatedComplaintIST);
     } catch (error) {
         console.error('Error updating complaint status:', error);
         res.status(500).json({ message: 'Failed to update complaint status', error: error.message });
@@ -86,5 +95,3 @@ app.get('/hi', (req, res) => {
 });
 
 module.exports = app;
-
-
